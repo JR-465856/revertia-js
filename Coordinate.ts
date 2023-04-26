652e5a8cedf828baa254212cf99a8995515ad8de
JR-465856 <97478088+JR-465856@users.noreply.github.com>
2023-04-26 09:55:15 -0700

    New vector math functions for Coordinate

--- Coordinate.ts
+++ Coordinate.ts
@@ -13,6 +13,28 @@
     // Get x and y
     public getX(): number { return this.x;}
     public getY(): number { return this.y;}
+
+    // Get length (vector)
+    public getLength(): number { return Math.sqrt((this.x*this.x)+(this.y*this.y));}
+    public getLengthSqr(): number { return (this.x*this.x)+(this.y*this.y);}
+
+    // Set length (vector)
+    public setLength(newLength:number): Coordinate {
+        const length = newLength/Math.sqrt((this.x*this.x)+(this.y*this.y));
+        return new Coordinate(
+            this.x*length,
+            this.y*length
+        );
+    }
+    
+    // Get normalized (vector)
+    public getNormal(): Coordinate {
+        const length = 1/Math.sqrt((this.x*this.x)+(this.y*this.y))
+        return new Coordinate(
+            this.x*length,
+            this.y*length
+        );
+    }
     
     // Arithmetic
     public add(other:Coordinate): Coordinate {
